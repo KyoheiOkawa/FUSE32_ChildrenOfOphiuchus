4b5d8370ad7dc3b4fedd942b75d9ca50e8a61217
KyoheiOkawa <kurokenken0310@gmail.com>
2016-12-03 21:36:18 +0900

    First commit

---
+++ Assets/Script/ResultDisplay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultDisplay : MonoBehaviour {
+
+	void Start () {
+
+        //this.GetComponent<Text>().text = "助けた人数" + people.GetScore().ToString() + "名";
+	}
+}
